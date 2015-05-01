ede39fc9a2846dcafb5b0fb84162c8ad915ef1f2
Conor Gilmer <conor.gilmer@gmail.com>
2015-05-01 13:52:44 +0100

    proportionality

--- uk/genlinegraphprop.php
+++ uk/genlinegraphprop.php
@@ -31,31 +31,44 @@ return $nameText;
 	
 // The Chart table contain two fields: Date and PercentageChange
 $queryData = mysql_query("
-  SELECT	date,
-                cons,
-                lab,
-                libdems,
-                ukip,
-                green,
-                snp,
-                pc,
-                uup,
-                dup,
-                sdlp,
-                sf,
-                others,
-                allseats
-        FROM seats_uk ");
+  SELECT	seats_uk.date as thedate,
+                seats_uk.cons as scons,
+                seats_uk.lab as slab,
+                seats_uk.libdems as slibdems,
+                seats_uk.ukip as sukip,
+                seats_uk.green as sgreen,
+                seats_uk.snp as ssnp,
+                seats_uk.pc as spc,
+                seats_uk.uup as suup,
+                seats_uk.dup as sdup,
+                seats_uk.sdlp as ssdlp,
+                seats_uk.sf as ssf,
+                seats_uk.others as sothers,
+                seats_uk.allseats as sallseats,
+  		vote_uk.date as vdate,
+                vote_uk.cons as vcons,
+                vote_uk.lab as vlab,
+                vote_uk.libdems as vlibdems,
+                vote_uk.ukip as vukip,
+                vote_uk.green as vgreen,
+                vote_uk.snp as vsnp,
+                vote_uk.pc as vpc,
+                vote_uk.uup as vuup,
+                vote_uk.dup as vdup,
+                vote_uk.sdlp as vsdlp,
+                vote_uk.sf as vsf,
+                vote_uk.others as vothers
+        FROM seats_uk, vote_uk where seats_uk.date = vote_uk.date");
 
 
-if ($choice == 'all') {
+/*if ($choice == 'all') {
 $table = array();
 $table['cols'] = array(
     array('label' => 'Date', 'type' => 'string'),
     array('label' => 'Conservatives', 'type' => 'number'),
     array('label' => 'Labour', 'type' => 'number'),
     array('label' => 'Liberal Democrats', 'type' => 'number'),
-    array('label' => 'Green', 'type' => 'number'),
+    array('label' => 'Greens', 'type' => 'number'),
     array('label' => 'UKIP', 'type' => 'number'),
     array('label' => 'SNP', 'type' => 'number'),
     array('label' => 'PC', 'type' => 'number'),
@@ -65,48 +78,53 @@ $table['cols'] = array(
     array('label' => 'SF', 'type' => 'number'),
     array('label' => 'Others', 'type' => 'number')
 );
-//First Series
+First Series
 $rows = array();
 while($r = mysql_fetch_assoc($queryData)) {
 	$temp = array();
-	// the following line will used to slice the Pie chart
+	 the following line will used to slice the Pie chart
 	$temp[] = array('v' => (string) $r['date']); 
 	$seats =$r['allseats'];
-	//Values of the each slice
-	$temp[] = array('v' => (float) (100* $r['cons']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['lab']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['libdems']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['green']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['ukip']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['ukip']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['snp']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['pc']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['uup']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['dup']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['sdlp']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['sf']/$seats)); 
-	$temp[] = array('v' => (float) (100* $r['others']/$seats)); 
+	xxValues of the each slice
+	$temp[] = array('v' => (float) (100 * $r['cons']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['lab']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['libdems']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['green']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['ukip']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['ukip']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['snp']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['pc']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['uup']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['dup']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['sdlp']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['sf']/$seats)); 
+	$temp[] = array('v' => (float) (100 * $r['others']/$seats)); 
 	$rows[] = array('c' => $temp);
 }
 }
-else {
+else {*/
 $table = array();
 $table['cols'] = array(
     array('label' => 'Date', 'type' => 'string'),
-    array('label' => getName($choice), 'type' => 'number')
+    array('label' => getName($choice), 'type' => 'number'),
+    array('label' => 'Proportional', 'type' => 'number')
 );
 //First Series
 $rows = array();
 while($r = mysql_fetch_assoc($queryData)) {
 	$temp = array();
+	//$sats =$r['allseats'];
 	// the following line will used to slice the Pie chart
-	$temp[] = array('v' => (string) $r['date']); 
+	$temp[] = array('v' => (string) $r['thedate']); 
 
 	//Values of the each slice
-	$temp[] = array('v' => (float) $r[$choice]); 
+$vchoice = "v".$choice;	
+$schoice = "s".$choice;	
+	$temp[] = array('v' => (float) ((100 * $r[$schoice])/650)); 
+	$temp[] = array('v' => (float) ($r[$vchoice])); 
 	$rows[] = array('c' => $temp);
 }
-}
+//}
 
 $table['rows'] = $rows;
 $jsonTable = json_encode($table);
@@ -114,9 +132,5 @@ $jsonTable = json_encode($table);
 echo $jsonTable;
 
 ?>
-
-
-
-
 
 
