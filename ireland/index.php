af65acfdc90ccb92a9b1e3ef3a14bb262cf1cc1f
Conor Gilmer <conor.gilmer@gmail.com>
2015-05-03 19:30:41 +0100

    home page

--- ireland/index.php
+++ ireland/index.php
@@ -1,3 +1,30 @@
 <?php include('php/header.php')?>
 <?php include('php/menu.php')?>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Irish Elections</h1>
+        <p class="lead">Looking at Opinion Polls, Votes, Seat returns and Proportionality of Irish Elections.</p>
+        <!--p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p-->
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>Opinion Pools</h2>
+          <p>The Line Chart shows how most of the parties have not changed much since 2007.</p>
+          <p><a class="btn btn-primary" href="polls.php" role="button">View details &raquo;</a></p>
+        </div>
+
+        <div class="col-lg-4">
+          <h2>Proportionality</h2>
+          <p>The crux of this is to look at how proportional is the representaion in a parliament from the votes cast for a particular party</p>
+          <p>The graph shows what percentage over or below of seats a party gets relative to its vote share, Ireland having Single Transferrable Vote - Proportional Representation, in all elections.</p>
+          <p><a class="btn btn-primary" href="prop.php" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+~                                                                                                                                                                             
+~                   
+
+
 <?php include('php/footer.php')?>
--- ireland/php/menu.php
+++ ireland/php/menu.php
@@ -18,26 +18,3 @@
           </ul>
         </nav>
       </div>
-
-      <!-- Jumbotron -->
-      <div class="jumbotron">
-        <h1>Irish Elections</h1>
-        <p class="lead">Looking at Opinion Polls, Votes, Seat returns and Proportionality of Irish Elections.</p>
-        <!--p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p-->
-      </div>
-
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-lg-4">
-          <h2>Opinion Pools</h2>
-          <p>The Line Chart shows how most of the parties have not changed much since 2007.</p>
-          <p><a class="btn btn-primary" href="polls.php" role="button">View details &raquo;</a></p>
-        </div>
-
-        <div class="col-lg-4">
-          <h2>Proportionality</h2>
-          <p>The crux of this is to look at how proportional is the representaion in a parliament from the votes cast for a particular party</p>
-          <p>The graph shows what percentage over or below of seats a party gets relative to its vote share..</p>
-          <p><a class="btn btn-primary" href="prop.php" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
