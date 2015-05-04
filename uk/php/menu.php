517ee16310058f252dae5deb17c58b2120798c17
Conor Gilmer <conor.gilmer@gmail.com>
2015-05-04 15:21:14 +0100

    framework

---
+++ uk/php/menu.php
@@ -0,0 +1,21 @@
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <!-- The justified navigation menu is meant for single line per list item.
+           Multiple lines will require custom code not provided by Bootstrap. -->
+      <div class="masthead">
+        <h3 class="text-muted">UK Elections and Polls</h3>
+        <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="polls.php">Polls</a></li>
+            <li><a href="votes.php">Votes</a></li>
+            <li><a href="seats.php">Seats</a></li>
+            <li><a href="prop.php">Proportionality</a></li>
+            <li><a href="about.php">About</a></li>
+          </ul>
+        </nav>
+      </div>
