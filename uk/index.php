319754c23f077e7f3a1b99c27eb75a63ec6a0986
Conor Gilmer <conor.gilmer@gmail.com>
2015-05-04 15:44:13 +0100

    broke up index

--- uk/index.php
+++ uk/index.php
@@ -1,52 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+<!-- header -->
+<?php include('php/header.php'); ?>  
 
-    <title>UK Elections and Polls</title>
+<!-- additional header stuff -->
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- menu -->
+<?php include('php/menu.php'); ?>  
 
-    <!-- Custom styles for this template -->
-    <link href="justified-nav.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <div class="container">
-
-      <!-- The justified navigation menu is meant for single line per list item.
-           Multiple lines will require custom code not provided by Bootstrap. -->
-      <div class="masthead">
-        <h3 class="text-muted">UK Elections and Polls</h3>
-        <nav>
-          <ul class="nav nav-justified">
-            <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="polls.php">Polls</a></li>
-            <li><a href="votes.php">Votes</a></li>
-            <li><a href="seats.php">Seats</a></li>
-            <li><a href="prop.php">Proportionality</a></li>
-            <li><a href="about.php">About</a></li>
-          </ul>
-        </nav>
-      </div>
+<!-- Content Start -->
 
       <!-- Jumbotron -->
       <div class="jumbotron">
@@ -72,15 +33,7 @@
         </div>
       </div>
 
-      <!-- Site footer -->
-      <footer class="footer">
-        <p>&copy; Conor Gilmer 2015</p>
-      </footer>
+<!-- Content End -->
 
-    </div> <!-- /container -->
-
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+<!-- footer -->
+<?php include('php/footer.php'); ?>  
