2b9a793b22d711fd6fc34b76671f9e3e29734960
Conor Gilmer <conor.gilmer@gmail.com>
2015-05-08 17:46:00 +0100

    irish polls

---
+++ ireland/getpolldata.php
@@ -0,0 +1,32 @@
+<?php
+   $q=$_GET["q"];
+  $dbuser="root";
+  $dbname="elections";
+  $dbpass="";
+  $dbserver="localhost";
+
+  $sql_query = "SELECT ff, fg, lab, green, sf, others, pd from polls_ireland where id = $q";
+  $con = mysql_connect($dbserver,$dbuser,$dbpass);
+  if (!$con){ die('Could not connect: ' . mysql_error()); }
+  mysql_select_db($dbname, $con);
+  $result = mysql_query($sql_query);
+  echo "{ \"cols\": [ {\"id\":\"\",\"label\":\"Party\",\"pattern\":\"\",\"type\":\"string\"}, {\"id\":\"\",\"label\":\"Percentage\",\"pattern\":\"\",\"type\":\"number\"} ], \"rows\": [ ";
+  $total_rows = mysql_num_rows($result);
+  $row_num = 0;
+  while($row = mysql_fetch_array($result)){
+    $row_num++;
+    if ($row_num == $total_rows){
+      echo "{\"c\":[{\"v\":\"" . 'Fianna Fail' . "\",\"f\":null},{\"v\":" . $row['ff'] . ",\"f\":null}]},";
+      echo "{\"c\":[{\"v\":\"" . 'Fine Gael' . "\",\"f\":null},{\"v\":" . $row['fg'] . ",\"f\":null}]},";
+      echo "{\"c\":[{\"v\":\"" . 'Labour' . "\",\"f\":null},{\"v\":" . $row['lab'] . ",\"f\":null}]},";
+      echo "{\"c\":[{\"v\":\"" . 'Sinn Fein' . "\",\"f\":null},{\"v\":" . $row['sf'] . ",\"f\":null}]},";
+      echo "{\"c\":[{\"v\":\"" . 'Green' . "\",\"f\":null},{\"v\":" . $row['green'] . ",\"f\":null}]},";
+      echo "{\"c\":[{\"v\":\"" . 'Progressive Democrats' . "\",\"f\":null},{\"v\":" . $row['pd'] . ",\"f\":null}]},";
+      echo "{\"c\":[{\"v\":\"" . 'Others' . "\",\"f\":null},{\"v\":" . $row['others'] . ",\"f\":null}]} ";
+    } else {
+      echo "{\"c\":[{\"v\":\"" . 'others' . "\",\"f\":null},{\"v\":" . $row['others'] . ",\"f\":null}]}, ";
+    }
+  }
+  echo " ] }";
+  mysql_close($con);
+?>
---
+++ ireland/polls.php
@@ -0,0 +1,81 @@
+<!-- header -->
+<?php include('php/header.php'); ?>  
+
+<!-- additional header stuff -->
+
+  <script type="text/javascript">
+
+                // Load the Visualization API and the chart package.
+                google.load('visualization', '1', {'packages':['corechart']});
+  function drawItems(num) {
+    var jsonChartData = $.ajax({
+      url: "genlinegraph.php",
+      data: "q="+num,
+      dataType:"json",
+      async: false
+    }).responseText;
+
+                        // Create our data table out of JSON data loaded from server.
+                        var data = new google.visualization.DataTable(jsonChartData);
+                        var options = {
+                                title: 'UK Opinion Polls of ' +num,
+                                width: 900,
+                                height: 500,
+				vAxis: {title: "Percentage"},
+                                hAxis: {title: "Date of Polls"}
+                        };
+                        // Instantiate and draw our chart, passing in some options.
+                        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+                        chart.draw(data, options);
+
+// a click handler which grabs some values then redirects the page
+        google.visualization.events.addListener(chart, 'select', function() {
+          // grab a few details before redirecting
+          var selection = chart.getSelection();
+          var row = selection[0].row;
+          var col = selection[0].column;
+          var year = data.getValue(row, 0);
+          location.href = 'pollspie.php?row=' + row + '&col=' + col + '&year=' + year;
+        });
+
+                }
+    </script>
+
+
+<!-- menu -->
+<?php include('php/menu.php'); ?>  
+
+
+<!-- Content Start -->
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-9">
+  
+<h1>Opinion Polls Ireland - Line Chart</h1>
+  <form>
+  <select name="users" onchange="drawItems(this.value)">
+  <option value="all">Select a Party:</option>
+    <option value="all">All</option>
+    <option value="ff">Fianna Fail</option>
+    <option value="fg">Fine Gael</option>
+    <option value="lab">Labour</option>
+    <option value="sf">Sinn Fein</option>
+    <option value="pd">Progressive Democrats</option>
+    <option value="green">Green</option>
+    <option value="rn">ReNua</option>
+    <option value="sp">Socialist Party</option>
+    <option value="pb">People Before Profit</option>
+    <option value="wp">Workers Party</option>
+    <option value="others">others</option>
+  </select>
+  </form>
+
+<div id="chart_div"></div>
+        </div>
+      </div>
+
+<!-- Content End -->
+
+<!-- footer -->
+<?php include('php/footer.php'); ?>  
