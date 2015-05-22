8b493c44a535535762277c8fdfbce10a6e1d8b48
Conor Gilmer <conor.gilmer@gmail.com>
2015-05-22 15:58:27 +0100

    avg low high all

--- uk/genlinegraphavg.php
+++ uk/genlinegraphavg.php
@@ -25,14 +25,38 @@ $queryData = mysql_query("
         FROM seats_uk ");
 
 
-if ($choice == 'all') {
 $table = array();
+if ($choice == 'all') {
 $table['cols'] = array(
     array('label' => 'Date', 'type' => 'string'),
     array('label' => 'High Bounce', 'type' => 'number'),
     array('label' => 'Low Bounce', 'type' => 'number'),
     array('label' => 'Average', 'type' => 'number')
 );
+} else if ($choice == 'low') {
+$table['cols'] = array(
+    array('label' => 'Date', 'type' => 'string'),
+    array('label' => 'Low', 'type' => 'number')
+);
+} else if ($choice == 'high') {
+$table['cols'] = array(
+    array('label' => 'Date', 'type' => 'string'),
+    array('label' => 'High', 'type' => 'number')
+);
+} else if ($choice == 'avg') {
+$table['cols'] = array(
+    array('label' => 'Date', 'type' => 'string'),
+    array('label' => 'Average', 'type' => 'number')
+);
+} else {
+$table['cols'] = array(
+    array('label' => 'Date', 'type' => 'string'),
+    array('label' => 'High Bounce', 'type' => 'number'),
+    array('label' => 'Low Bounce', 'type' => 'number'),
+    array('label' => 'Average', 'type' => 'number')
+);
+
+}
 //First Series
 $rows = array();
 while($r = mysql_fetch_assoc($queryData)) {
@@ -44,57 +68,58 @@ while($r = mysql_fetch_assoc($queryData)) {
 	$highbounce  = 0;
 	$lowbounce   = 100.0;
 	$avgbounce   = 0;
-	$count       = 1;
 	$bounce      = 0;
 	$bounce_t    = 0;
 	$parlseats   = 650;
 	while($votes = mysql_fetch_assoc($queryDataV)) {
 		$bounce = (float)($r['cons']) - ($parlseats * $votes['cons']/100);
 		$bounce_t = abs($bounce) + $bounce_t;
-		//if ($lowbounce > (abs($bounce))) { 
-		//	$lowbounce = $bounce;
-		//}
-		$lowbounce = (((abs($bounce) > $lowbounce)) ? $lowbounce : abs($bounce));
+		$lowbounce = (((($bounce) > $lowbounce)) ? $lowbounce :($bounce));
 		$highbounce = (((abs($bounce) < $highbounce)) ? $highbounce : abs($bounce));
-	//	echo $lowbounce . "<br>";
-		$bounce = $r['lab'] - ($parlseats * $votes['lab']/100); 
+		
+		$bounce = (float)$r['lab'] - ($parlseats * $votes['lab']/100); 
 		$lowbounce = (((abs($bounce) > $lowbounce)) ? $lowbounce : abs($bounce));
 		$highbounce = (((abs($bounce) < $highbounce)) ? $highbounce : abs($bounce));
 		$bounce_t = abs($bounce) + $bounce_t; 
-		$bounce = $r['libdems'] - ($parlseats * $votes['libdems']/100); 
-		$lowbounce = (((abs($bounce) > $lowbounce)) ? $lowbounce : abs($bounce));
+		
+		$bounce = (float)$r['libdems'] - ($parlseats * $votes['libdems']/100); 
+		$lowbounce = (((($bounce) > $lowbounce)) ? $lowbounce : ($bounce));
 		$highbounce = (((abs($bounce) < $highbounce)) ? $highbounce : abs($bounce));
 		$bounce_t = abs($bounce) + $bounce_t; 
-		$count = $count +1;
+		
+		$bounce = (float)$r['snp'] - ($parlseats * $votes['snp']/100); 
+		$lowbounce = (((($bounce) > $lowbounce)) ? $lowbounce : ($bounce));
+		$highbounce = (((abs($bounce) < $highbounce)) ? $highbounce : abs($bounce));
+		$bounce_t = abs($bounce) + $bounce_t; 
+		
+		$bounce = (float)$r['ukip'] - ($parlseats * $votes['ukip']/100); 
+		$lowbounce = (((($bounce) > $lowbounce)) ? $lowbounce : ($bounce));
+		$highbounce = (((abs($bounce) < $highbounce)) ? $highbounce : abs($bounce));
+		$bounce_t = abs($bounce) + $bounce_t; 
 	}
 	//Values of the each slice
+	if ($choice == 'all') {
 	$temp[] = array('v' => (float) ($highbounce )); 
 	$temp[] = array('v' => (float) ($lowbounce )); 
-	$temp[] = array('v' => (float) ($bounce_t)/3.0); 
+	$temp[] = array('v' => (float) ($bounce_t)/5.0); 
+	}
+	else if ($choice == 'low') {
+	$temp[] = array('v' => (float) ($lowbounce )); 
+	}
+	else if ($choice == 'high') {
+	$temp[] = array('v' => (float) ($highbounce )); 
+	}
+	else if ($choice == 'avg') {
+	$temp[] = array('v' => (float) ($bounce_t)/5.0); 
+	}
+	else  {
+	$temp[] = array('v' => (float) ($highbounce )); 
+	$temp[] = array('v' => (float) ($lowbounce )); 
+	$temp[] = array('v' => (float) ($bounce_t)/5.0); 
+	}
 	$rows[] = array('c' => $temp);
 }
-}
-else {
-$table = array();
-$table['cols'] = array(
-    array('label' => 'Date', 'type' => 'string'),
-    array('label' => $choice, 'type' => 'number')
-);
-//First Series
-$rows = array();
-while($r = mysql_fetch_assoc($queryData)) {
-	$temp = array();
-	// the following line will used to slice the Pie chart
-	$temp[] = array('v' => (string) $r['date']); 
-
-	//Values of the each slice
-	$temp[] = array('v' => (float) $r[$choice]);
-
-
- 
-	$rows[] = array('c' => $temp);
-}
-}
+//}
 
 $table['rows'] = $rows;
 $jsonTable = json_encode($table);
