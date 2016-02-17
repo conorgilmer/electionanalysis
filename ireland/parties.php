8ab2b66eb5704353abe5e08913bc6c579c4a2afd
Conor Gilmer <conor.gilmer@gmail.com>
2016-02-17 12:06:30 +0000

    parties

--- ireland/parties.php
+++ ireland/parties.php
@@ -9,7 +9,53 @@
       <div class="row">
         <div class="col-lg-9">
           <h2>Political Parties</h2>
-          <p>An overview of Irish political parties</p>
+          <p>An overview of Irish political parties who are running candidates in the 2016 general election.</p>
+	  
+	  <h3>Fianna Fail</h3>
+	  <p>Center Catch all Party founded in 1927 by Eamon De Valera out of the anti-treaty side from the civil war. First elected as government in 1932, and been in power for the majority of the time since. Devestated in the 2011 election reduced to 20 seats.  Its leader is Micheal Martin TD.
+          </p>
+
+	  
+	  <h3>Fine Gael</h3>
+	  <p>Center Right political party founded from a merger of the Cumann na nGaedheal(Pro-treaty side), National Centre Party and the National Guard (Blueshirts) in 1933. Affiliated to Christan Democrats in Europe, been in government in coalition with Labour and other parties over time. Largest party in the Dail and opinion polls since the 2011 election. Its leader is Enda Kenny TD.
+          </p>
+
+	 <h3>Green Party</h3>
+	<P>Founded in 1981 as the ecology party, it used the moniker Green Alliances before using the name Green Party. First elected a leader in 2001, it was in a coalition with Fianna Fail from 2007 until 2011, when it lost all its TDs in the election. Its present leader is former TD Eamon Ryan.
+</p>
+	  <h3>Labour</h3>
+	  <p>Centre Left party founded in 1912 by James Connolly, James Larkin and William O'Brien. Has formed governments with Fine Gael in coalition and in the multi party governments. Has been subject to splits (National Labour 1944-1950) and mergers Democratic Left (1999). In government with Fine Gael since 2011 when it had its best electoral result ever, Its leader is Joan Burton TD.
+          </p>
+
+
+	  <h3>Renua Ireland</h3>
+	  <p>Founded in 2015 from three former Fine Gael TDs and Senator who lost the party whip over Abortion, which they seen as breaking the election promise the party had made. Its leader is the former european minister Lucinda Creighton TD.</p>
+
+	  <h3>Sinn Fein</h3>
+	  <p>All-Ireland political party, claims it is the direct connection to Arthur Griffiths Sinn Fein in 1905, but the present party originates from Provisional wing in the 1970 split which also created the Offical Sinn Fein/Workers Party. In power sharing arrangement in the North of Ireland. Has been represented in the Dail since 1997, and presently has 14 tds, its leader is Gerry Adams TD.
+          </p>
+
+	  <h3>Social Democrats</h3>
+	  <p>Founded in 2015 from three Independent TDs, Stephen Donnelly elected as an Independent, Catherine Murphy an Independent TD who was formerly in the Workers Party, and Labour Party before winning a bye-election as an Indpendent, and regaining her seat in 2011, and Roisin Shorthall TD who left the labour party over Health minister allocating funding to projects in his constituency.
+</p>
+	  <h3>Anti-Austerity Alliance - People Before Profit</h3>
+	  <p>Founded in 2016, from an alliance of alliances to fight the general election. the AAA is an Alliance formed mainly from Socialist Party members, and the People Before Profit were formed largely from members of the Socialist Workers Party. Offically no leader, the party has 4 TDs, one from PBP and on from AAA/SP. In 2011 there was a similar alliance, called got Clare Daly and Joan Collins now indpendent, elected.
+</p>
+	<h3>Independent Alliance</h3>
+	<p>Formed in 2015 with an eye on the 2016 election by Independent TD's Shane Ross and Michael Fitzmaurice. It currently has five TD's and two Senators.</p>
+  
+	<h3>Workers Party</h3>
+	<p>Originated from the split in Sinn Fein in 1970, the party migrated from Official Sinn Fein, to Sinn Fein the Workers Party to The Workers Party. Greatly diminshed since the Split in 1992 which seen the party lose 6 TD's who formed the Democtatic Left. Running a number of candidates in the 2016 election.</p>
+ 
+	<h3>Direct Democracy Ireland</h3>
+	<p>Idea based on swiss participatory democracy.</p>
+
+	<h3>Peoples Convention</h3>
+	<p>Endorses a number of Independents in the 2016 election.</p>
+ 
+	<h3>Postmasters Union</h3>
+	<p>Running a small number of candidates in the 2016 election.</p>
+ 
       </div>
       </div>
 
