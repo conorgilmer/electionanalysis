f8edec56b9f5cdbb2447b9bb88e8d9f0da31d968
Conor Gilmer <conor.gilmer@gmail.com>
2016-11-28 11:19:49 +0000

    fix typo

--- uk/about.php
+++ uk/about.php
@@ -9,7 +9,7 @@
       <div class="row">
         <div class="col-lg-9">
           <h2>About</h2>
-          <p>Well I am a political(electoral system) geek, I love looking at elections in countries and seeing how they reflect what the people want etc. No system is perfect, and some good systems produce bad results and viceversa</ref>.
+          <p>Well I am a political(electoral system) geek, I love looking at elections in countries and seeing how they reflect what the people want etc. No system is perfect, and some good systems produce bad results and viceversa</p>.
           <p>In the run up to the May 2015, General Election in the UK, i threw up these pages showing the polls, and the ratio of seats in the legislature to the votes a party got.</p>
 	<p>The UK for Westminister elections uses the <strong>First past the post (FPTP)</strong> system, which favours the larger parties, and often excludes minor parties from the parliament, and minimises the third party..</p>
 
