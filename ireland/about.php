cf9246f0fc6695640d44723ff29f247f271db30b
Conor Gilmer <conor.gilmer@gmail.com>
2018-09-25 12:36:46 +0100

    updates

--- ireland/about.php
+++ ireland/about.php
@@ -9,11 +9,11 @@
       <div class="row">
         <div class="col-lg-9">
           <h2>About</h2>
-          <p>Well I am a political(electoral system) geek, I love looking at elections in countries and seeing how they reflect what the people want etc. No system is perfect, and some good systems produce bad results and viceversa</ref>.
+          <p>Well I am a political (electoral system) geek, I love looking at elections in countries and seeing how they reflect what the people want etc. No system is perfect, and some good systems produce bad results and viceversa</ref>.
           <p>Here I look at Irish general elections which use Proportional Representation - Single Transferrable Vote(PR-STV).</p>
 	<p>I have grouped together Sinn Fein(1918/1921), Anti-Treaty-SF, Republicans and Fianna Fail</p>
 	<p>I have grouped together Pro-Treaty-SF, Cumann na nGaedheal and Fine Gael</p>
-	<p>I have aslo grouped together Offical SF, SFWP and WP, as well as Abstentionist SF, PSF, HBlock and Sinn Fein</p>
+	<p>I have aslo grouped together Offical SF, SFWP and WP, as well as Abstentionist SF, PSF, H-Block and Sinn Fein</p>
 	<p>Unionists I have kept, but perhaps I could interpret independents and map them with the Irish Unionists, such as the Trinity College TD up until 1937.</p>
 
         <p>Others/Independents in opinion polls often includes minor parties, in some cases the green party, workers party, socialist party, people before profit, and more recently Renua and Social democrats</p>
