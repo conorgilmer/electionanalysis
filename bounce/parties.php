ed5415a6653d476de4da1462573eb4cf3846aad5
Conor Gilmer <conor.gilmer@gmail.com>
2018-11-13 15:26:51 +0000

    added election bounce other pages

---
+++ bounce/parties.php
@@ -0,0 +1,11 @@
+<?php include 'headercon.php'; ?>
+<p>
+</p>
+<h2>Party Seats</h2>
+<p>
+
+<img background-color="transparent"  src="partyseats.php">
+
+</p>
+
+<?php include 'footercon.php'; ?>
---
+++ bounce/seatbounce2011.php
@@ -0,0 +1,131 @@
+<?php
+ $page_title = "General Election 2011";
+ include 'header.php'; 
+?>
+<p>
+
+<h2>Ireland PR-STV General Election 2011- % Seat Bonus</h2>
+<table border=0>
+<tr><td colspan="2">
+<img src="bounceirege2011.php">
+</td></tr></table>
+<TABLE CELLSPACING=0 COLS=6 BORDER=1 width="650">
+	<TBODY>
+		<TR>
+			<TD ALIGN="Left" ><B>Party</B></TD>
+			<TD ALIGN="CENTER" ><B>1st Pref Vote %</B></TD>
+			<TD ALIGN="CENTER" ><B>Seats</B></TD>
+			<TD ALIGN="CENTER" ><B>% of Seats</B></TD>
+			<TD ALIGN="CENTER" ><B>Seats Due</B></TD>
+			<TD ALIGN="CENTER" ><B>Difference/Bounce</B></TD>
+		</TR>
+		<TR>
+			<TD ALIGN="Left" >Fianna F&aacute;il</TD>
+			<TD ALIGN="CENTER" SDVAL="17.4" >17.4</TD>
+			<TD ALIGN="CENTER" SDVAL="20" >20</TD>
+			<TD ALIGN="CENTER" SDVAL="12.0481927710843" >12.05</TD>
+			<TD ALIGN="CENTER" SDVAL="28.884" >29</TD>
+			<TD ALIGN="CENTER" SDVAL="-5.35180722891566" >-5.35</TD>
+		</TR>
+		<TR>
+			<TD ALIGN="Left" >Fine Gael</TD>
+			<TD ALIGN="CENTER" SDVAL="36.1" >36.1</TD>
+			<TD ALIGN="CENTER" SDVAL="76" >76</TD>
+			<TD ALIGN="CENTER" SDVAL="45.7831325301205" >45.78</TD>
+			<TD ALIGN="CENTER" SDVAL="59.926" >60</TD>
+			<TD ALIGN="CENTER" SDVAL="9.68313253012048" >9.68</TD>
+		</TR>
+		<TR>
+			<TD ALIGN="Left" >Labour</TD>
+			<TD ALIGN="CENTER" SDVAL="19.4" >19.4</TD>
+			<TD ALIGN="CENTER" SDVAL="37" >37</TD>
+			<TD ALIGN="CENTER" SDVAL="22.289156626506" >22.29</TD>
+			<TD ALIGN="CENTER" SDVAL="32.204" >32</TD>
+			<TD ALIGN="CENTER" SDVAL="2.88915662650603" >2.89</TD>
+		</TR>
+		<TR>
+			<TD ALIGN="Left" >Sinn F&eacute;in</TD>
+			<TD ALIGN="CENTER" SDVAL="9.9" >9.9</TD>
+			<TD ALIGN="CENTER" SDVAL="14" >14</TD>
+			<TD ALIGN="CENTER" SDVAL="8.43373493975904" >8.43</TD>
+			<TD ALIGN="CENTER" SDVAL="16.434" >16</TD>
+			<TD ALIGN="CENTER" SDVAL="-1.46626506024096" >-1.47</TD>
+		</TR>
+		<TR>
+			<TD ALIGN="Left" >Green Party</TD>
+			<TD ALIGN="CENTER" SDVAL="1.8" >1.8</TD>
+			<TD ALIGN="CENTER" SDVAL="0" >0</TD>
+			<TD ALIGN="CENTER" SDVAL="0" >0</TD>
+			<TD ALIGN="CENTER" SDVAL="2.988" >3</TD>
+			<TD ALIGN="CENTER" SDVAL="-1.8" >-1.8</TD>
+		</TR>
+		<TR>
+			<TD ALIGN="Left" >Socialist Party </TD>
+			<TD ALIGN="CENTER" SDVAL="1.2" >1.2</TD>
+			<TD ALIGN="CENTER" SDVAL="2" >2</TD>
+			<TD ALIGN="CENTER" SDVAL="1.20481927710843" >1.2</TD>
+			<TD ALIGN="CENTER" SDVAL="0.725794745246044" >1</TD>
+			<TD ALIGN="CENTER" SDVAL="0.00481927710843388" >0</TD>
+		</TR>
+		<TR>
+			<TD ALIGN="Left" >People Before Profit</TD>
+			<TD ALIGN="CENTER" SDVAL="1" >1</TD>
+			<TD ALIGN="CENTER" SDVAL="2" >2</TD>
+			<TD ALIGN="CENTER" SDVAL="1.20481927710843" >1.2</TD>
+			<TD ALIGN="CENTER" SDVAL="0.725794745246044" >1</TD>
+			<TD ALIGN="CENTER" SDVAL="0.204819277108434" >0.2</TD>
+		</TR>
+		<TR>
+			<TD ALIGN="Left" >Others</TD>
+			<TD ALIGN="CENTER" SDVAL="13.2" >13.2</TD>
+			<TD ALIGN="CENTER" SDVAL="15" >15</TD>
+			<TD ALIGN="CENTER" SDVAL="9.03614457831325" >9.04</TD>
+			<TD ALIGN="CENTER" SDVAL="21.912" >22</TD>
+			<TD ALIGN="CENTER" SDVAL="-4.16385542168675" >-4.16</TD>
+		</TR>
+		<TR>
+			<TD ALIGN="CENTER" ><BR></TD>
+			<TD ALIGN="CENTER" SDVAL="100" >100</TD>
+			<TD ALIGN="CENTER" SDVAL="166" >166</TD>
+			<TD ALIGN="CENTER" SDVAL="100" >100</TD>
+			<TD ALIGN="CENTER" > </TD>
+			<TD ALIGN="CENTER" SDVAL="0" >0</TD>
+		</TR>
+	</TBODY>
+</TABLE>
+
+<h2>Ireland PR-STV General Election 2011 Seat Bonus</h2>
+<table border=0>
+<tr><td colspan="2">
+<img src="bounceirege2011s.php">
+</td></tr></table>
+
+
+</p></p>
+The 2011 General Election was a Sea Change in Irish Politics with the decline of the Fianna F&aacute;il Vote to its lowest ever. It was also the most successful election in seats returned for Fine Gael. And the most successful for the modern day Sinn F&eacute;in.
+Other things to note was the massive increase in the Labour party percentage of the vote and seats returned (while slightly less than the 1992 election if you combine the Democratic Left and the Labour party they returned the same amount of seats. Another Casualty of this election has been the Green party which lost all its seats).
+</p><p>
+The big winner on seats was Fine Gael and the big loser was Fianna F&aacute;il (who have benefitted a great deal in recent elections from a Seat bounce) this is down to the transfer unfriendliness of Fianna F&aacute;il particularly in lower preference votes, but also in running two many candidates in the case of Donegal South West, Dublin South East, Dun Laoghaire and perhaps Wicklow where a single candidate would have stayed in the race longer.</p>
+
+Note: Above the figures of the Socialist Party and People Before Profit are separate, since the ULA is note registered as a political party and some of its candidates were listed as non-party are included in the independents/others e.g. Seamus Healy TD.
+<p>
+Could Fine Gael have got an overall majority, well it would have been extremely unlikely, they did however have excellent vote management (Mayo, Wicklow, Dublin South, Cavan-Monaghan, Meath East, Meath West & Louth), but in some cases they missed seats, Dublin North West they ran two many candidates, where they didnt have a seat, and it is the only constituency where they dont have a seat, Dublin South Central they alow ran to many candidates and only got one seat (a 4 candidate strategy in Galway West nearly cost them a seat as well), but Galway East, Cork South Central, Clare, and to a lesser extent Tipperary South, Kerry North and South as well as Kildare South with a 2nd candidate with a bit of luck they could have taken extra seats. Labours change of tone in the last week of trying to prevent a single party government may also have helped prevent an overall majority
+</p>
+<hr>
+
+<h2>Some Previous Elections in Ireland PR-STV</h2>
+<table border=0>
+<tr><td>
+<img src="bounceirege2007.php">
+</td><td>
+<img src="bounceirege1992.php">
+</td></tr></table></p>
+<p>
+The Irish PR system does give a relative reflection of votes to seats achieved by the parties, with the larger parties getting a bonus, the PD's seemed to switch between getting a seat bonus and being under represented through out their existence, while Sinn F&eacute;in has got seats in the D&aacute;il they have been traditionally <i>"transfer repellant"</i> and had to rely on large 1st preference votes to get elected since they don't pick up transfers.
+<hr>
+<p>
+<h2>Voting Systems First Past the Post versus Proportional Representation - Single Transferable Vote (PR-STV) </h2>
+Click on this  <a target="_blank" href="bounce.php">link</a> where i looked at the UK system in general elections and its proportional system for European Elections and some Irish general elections.
+
+</p>
+<?php include 'footer.php'; ?>
