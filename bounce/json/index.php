9105dd5c72dce9aae91c1720991778940454d711
Conor Gilmer <conor.gilmer@gmail.com>
2018-11-19 18:52:49 +0000

    added bs json

---
+++ bounce/json/index.php
@@ -0,0 +1,9 @@
+<?php include('header.php');?>
+      <div class="jumbotron">
+        <h2>Electoral Systems</h2>
+        <p class="lead">Electoral Systems Seat Bonus</p>
+        <p><a class="btn btn-lg btn-success" href="uk.php">First Past the Post (UK)</a></p>
+        <p><a class="btn btn-lg btn-success" href="ireland.php">Single Transferrable Voter (Ireland)</a></p>
+      </div>
+
+<?php include('footer.php');?>
